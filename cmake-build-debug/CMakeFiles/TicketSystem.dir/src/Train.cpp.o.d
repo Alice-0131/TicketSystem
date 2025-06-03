300dd7ef1a50b833eef728481a0f033595695308
Jiaqi Xu <dlrbdlmltzzn@sjtu.edu.cn>
2025-06-04 01:56:43 +0800

    update

--- cmake-build-debug/CMakeFiles/TicketSystem.dir/src/Train.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/TicketSystem.dir/src/Train.cpp.o: \
- /mnt/c/Users/阿丝儿心心/CLionProjects/TicketSystem/src/Train.cpp \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/阿丝儿心心/CLionProjects/TicketSystem/src/../include/Train.hpp
